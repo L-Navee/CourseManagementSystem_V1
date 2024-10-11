99bb6fabebbf42f9ff659a637684164c33864df6
L-Navee <naveethannaveethan13@gmail.com>
2024-10-11 16:35:18 +0530

    Question6-b

---
+++ CourseManagementSystem_V1/OnPremisCourse.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseManagementSystem_V1
+{
+    internal class OnPremisCourse:Course
+    {
+        public string Schedule;
+        public int ClassroomCapacity;
+
+        public OnPremisCourse(string courseId, string title, string duration, decimal price, string schedule, int classroomCapacity) : base(courseId, title, duration, price)
+        {
+            this.Schedule = schedule;
+            this.ClassroomCapacity = classroomCapacity; 
+        }
+        //public DisplayOnPremisCourseInfo()
+        //{
+        //    return $"CourseId:{CourseId},Title:{Title},Duration:{Duration},Price:{Price},Schedule:{Schedule},Classroomcapacity:{ClassroomCapacity}";
+        //}
+    }
+}
