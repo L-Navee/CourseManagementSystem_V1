04568ae6a814660d85bc77913eabf6fab72659dd
L-Navee <naveethannaveethan13@gmail.com>
2024-10-11 15:07:33 +0530

    Question 6-a

---
+++ CourseManagementSystem_V1/DigitalCourse.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseManagementSystem_V1
+{
+    internal class DigitalCourse:Course
+
+    {
+
+        public string CourseLink;
+        public double FileSize;
+
+        public DigitalCourse(string courseId, string title, string duration, decimal price, string courseLink, double fileSize) : base(courseId, title, duration, price)
+        {
+            this.CourseLink = courseLink;
+            this.FileSize = fileSize;
+        }
+        
+    }
+}
